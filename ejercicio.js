efb34a79b4e30837345ec35b00a1fdc935f39e39
JhonIsNotReal <110693901+Jhonisnotreal@users.noreply.github.com>
2022-11-11 23:59:20 -0600

    Add files via upload

---
+++ ejercicio.js
@@ -0,0 +1,13 @@
+ejercicio = ["Juan Janett", 19, false]
+
+const fecha_cumple = new Date("december 17 2002");
+console.log(fecha_cumple)
+
+const Librofavorito = {
+    titulo: "El Psicoanalista",
+    autor: "John Katzenbach",
+    fecha: [29, 01, 2002],
+    // url: https://es.wikipedia.org/wiki/El_psicoanalista
+}
+
+console.log(Librofavorito)
